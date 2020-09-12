d9420033267cd9cf4ebc0cf02a326f72d16c850d
userx <userx@userxs-mbp>
2020-09-12 16:42:36 -0400

    add a chrome node and a chrome test

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Here are the steps to setup the docker containers...
 	docker run -d -p 4545:4444 --name selenium-hub selenium/hub  
 	docker pull selenium/node-firefox-debug    
 	docker run -d --link selenium-hub:hub selenium/node-firefox     
+	docker run -d --link selenium-hub:hub selenium/node-chrome     
+	
 
 Verify by going here...  
 http://localhost:4545/grid/console  
 You should see a firefox node.  
 
 Then you can just run the test :)
+
+Optionally you can do this instead ...  
+	
+	docker run -d --link selenium-hub:hub selenium/node-firefox-debug      
+	docker run -d --link selenium-hub:hub selenium/node-chrome-debug    
--- src/test/java/sample/GoogleRemoteIT.java
+++ src/test/java/sample/GoogleRemoteIT.java
@@ -2,26 +2,49 @@ package sample;
 
 import java.net.URL;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class GoogleRemoteIT {
+	
+	String nodeURL = "http://localhost:4545/wd/hub"; //http://192.168.50.136:4545/grid/console
+	
+	WebDriver fireFoxWebDriver = null;
+	WebDriver chromeWebDriver = null;
+	
+	@BeforeEach public void setup() throws Exception {
+		DesiredCapabilities fireFoxCapabilities = DesiredCapabilities.firefox();
+		fireFoxCapabilities.setBrowserName("firefox");
+		fireFoxCapabilities.setPlatform(Platform.LINUX);
+		fireFoxWebDriver = new RemoteWebDriver(new URL(nodeURL), fireFoxCapabilities);   
+		
+		DesiredCapabilities chromeCapabilities = DesiredCapabilities.chrome();
+		chromeCapabilities.setBrowserName("chrome");
+		chromeCapabilities.setPlatform(Platform.LINUX);
+		chromeWebDriver = new RemoteWebDriver(new URL(nodeURL), chromeCapabilities);   
+	}
+	
 	@Test
-	@DisplayName("simple google grid test")
-	public void test() throws Exception{
-		String nodeURL = "http://localhost:4545/wd/hub"; //http://192.168.50.136:4545/grid/console
-		FirefoxOptions ffOptions = new FirefoxOptions();
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setBrowserName("firefox");
-		capabilities.setPlatform(Platform.LINUX);
-		WebDriver driver = new RemoteWebDriver(new URL(nodeURL), capabilities);   
-		driver.get("http://www.google.com");
-		driver.quit();
+	@DisplayName("simple google ff grid test")
+	public void testFirefox() throws Exception{
+		fireFoxWebDriver.get("http://www.google.com");
+	}
+	
+	@Test
+	@DisplayName("simple google chr grid test")
+	public void testChrome() throws Exception{
+		chromeWebDriver.get("http://www.google.com");
+	}
+	
+	@AfterEach public void teardown() {
+		fireFoxWebDriver.quit();
+		chromeWebDriver.quit();
 	}
 }
 
